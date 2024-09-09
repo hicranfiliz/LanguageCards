388508ce03255a38ad4596f19924f1c67fa62f7e
hicranfiliz <h.filiz781@gmail.com>
2024-09-09 23:01:51 +0300

    added viewBinding configuration

--- app/src/main/java/com/example/languagecards/MainActivity.kt
+++ app/src/main/java/com/example/languagecards/MainActivity.kt
@@ -7,14 +7,19 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.example.languagecards.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -23,7 +28,6 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        val navView : BottomNavigationView = findViewById(R.id.bottomNavigation)
-        navView.setupWithNavController(navController)
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
